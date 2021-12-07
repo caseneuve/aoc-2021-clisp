ad74a0d97a306054add8b617a3eef1734ecabd89
Piotr Kaznowski <piotrkaznowski@gmail.com>
2021-12-07 22:13:42 +0100

    day 5 refactored by using hashmap instead of a list of lists

--- 05.lisp
+++ 05.lisp
@@ -1,59 +1,54 @@
+;; --- Day 5: Hydrothermal Venture ---
+
 (load "aoc")
 
 (defparameter +day5+ (mapcar #'aoc:str->ints (uiop:read-file-lines "05.in")))
 
 ;; Helpers
-(defun parse-input (input)
-  (loop with maxx = 0
-        with maxy = 0
-        for (x1 y1 x2 y2) in input
-        do (setf maxx (max maxx x1 x2) maxy (max maxy y1 y2))
-        collect (list x1 y1 x2 y2) into crd
-        finally (return (values crd maxx maxy))))
+(defun mark-vertical (x1 x2 y1 ht)
+  (loop for x from (min x1 x2) to (max x1 x2) do (incf (gethash (cons x y1) ht 0))))
 
-(defun make-diagram (maxx maxy)
-  (loop for y from 0 to maxy collect (make-list (1+ maxx) :initial-element 0)))
+(defun mark-horizontal (y1 y2 x1 ht)
+  (loop for y from (min y1 y2) to (max y1 y2) do (incf (gethash (cons x1 y) ht 0))))
 
-(defun mark-vertical (x1 x2 y1 diagram)
-  (loop with row = (nth y1 diagram)
-        for x from (min x1 x2) to (max x1 x2)
-        do (incf (nth x row))))
-
-(defun mark-horizontal (x1 y1 y2 diagram)
-  (loop for y from (min y1 y2) to (max y1 y2)
-        for row = (nth y diagram)
-        do (incf (nth x1 row))))
-
-(defun mark-diagonal (x1 x2 y1 y2 diagram)
+(defun mark-diagonal (x1 x2 y1 y2 ht)
   (loop
+    for x from (min x1 x2) to (max x1 x2)
     with y = (if (= x1 (min x1 x2)) y1 y2)
     with fn = (if (< y (max y1 y2)) '1+ '1-)
-    for x from (min x1 x2) to (max x1 x2)
-    do (incf (nth x (nth y diagram)))
+    do (incf (gethash (cons x y) ht 0))
        (setf y (funcall fn y))))
 
-(defun update-diagram (coordinates diagram &key part2)
+(defun count-overlapping (ht)
+  (loop for v being the hash-values of ht
+        with counter = 0
+        if (> v 1) do (incf counter)
+        finally (return counter)))
+
+(defun update-diagram! (coordinates ht &key part2)
   (loop
     for (x1 y1 x2 y2) in coordinates
-    if      (= y1 y2) do (mark-vertical   x1 x2 y1    diagram)
-    else if (= x1 x2) do (mark-horizontal x1 y1 y2    diagram)
-    else if part2     do (mark-diagonal   x1 x2 y1 y2 diagram)
-    finally           (return diagram)))
+    if      (= y1 y2) do (mark-vertical   x1 x2 y1 ht)
+    else if (= x1 x2) do (mark-horizontal y1 y2 x1 ht)
+    else if part2     do (mark-diagonal   x1 x2 y1 y2 ht)
+    finally (return ht)))
 
-(defun count-overlapping-lines (diagram)
-  (length (remove-if (lambda (n) (< n 2)) (apply #'append diagram))))
 
 ;; Solution
-(multiple-value-bind (coordinates maxx maxy) (parse-input +day5+)
+(let ((diagram1 (make-hash-table :test #'equal))
+      (diagram2 (make-hash-table :test #'equal)))
   (aoc:solve
-   (count-overlapping-lines (update-diagram coordinates (make-diagram maxx maxy)))
-   (count-overlapping-lines (update-diagram coordinates (make-diagram maxx maxy) :part2 t))))
+   (count-overlapping (update-diagram! +day5+ diagram1))
+   (count-overlapping (update-diagram! +day5+ diagram2 :part2 t))))
+
 
 ;; Tests
 (use-package :lisp-unit)
 
 (define-test day5
-  (let ((input (mapcar #'aoc:str->ints '("0,9 -> 5,9"
+  (let ((diagram1 (make-hash-table :test #'equal))
+        (diagram2 (make-hash-table :test #'equal))
+        (input (mapcar #'aoc:str->ints '("0,9 -> 5,9"
                                          "8,0 -> 0,8"
                                          "9,4 -> 3,4"
                                          "2,2 -> 2,1"
@@ -63,11 +58,7 @@
                                          "3,4 -> 1,4"
                                          "0,0 -> 8,8"
                                          "5,5 -> 8,2"))))
-    (assert-equal 5
-     (multiple-value-bind (coordinates maxx maxy) (parse-input input)
-       (count-overlapping-lines (update-diagram coordinates (make-diagram maxx maxy)))))
-    (assert-equal 12
-     (multiple-value-bind (coordinates maxx maxy) (parse-input input)
-       (count-overlapping-lines (update-diagram coordinates (make-diagram maxx maxy) :part2 t))))))
+    (assert-equal  5 (count-overlapping (update-diagram! input diagram1 )))
+    (assert-equal 12 (count-overlapping (update-diagram! input diagram2 :part2 t)))))
 
 (run-tests '(day5))
